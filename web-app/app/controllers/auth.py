3001e92968ca66bb6cc94ef23320898e5157c0d6
Charlie Crouse <crouse0@purdue.edu>
2017-11-20 12:51:10 -0500

    updates to rest api model

--- web-app/app/controllers/auth.py
+++ web-app/app/controllers/auth.py
@@ -1,12 +1,10 @@
-import json
+import binascii
+import os
 from flask import jsonify
 from app import app, db
-from app.views.auth import SignupForm, LoginForm
 from app.models.user import User, Role
-
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import Blueprint, request
 from flask_security import Security, SQLAlchemyUserDatastore
-from flask_security import current_user, login_user, login_required, logout_user
 from flask_security.utils import hash_password, verify_password
 
 auth = Blueprint('auth', __name__)
@@ -17,90 +15,38 @@ user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
 
-@auth.route('/signup', methods=['GET', 'POST'])
-def signup_page():
-    """Displays the signup page"""
-    form = SignupForm(request.form)
+def validate_request(request):
+    """Validates a http request"""
+    data = request.get_json()
 
-    if request.method == 'GET':
-        return render_template('auth/signup.html', form=form)
+    email = data['email']
+    token = data['token']
 
-    # validate the user's input
-    if not form.validate():
-        flash('error Invalid Input Provided. Please Try Again.')
-        return redirect(url_for('auth.signup_page'))
+    user = User.query.filter_by(email=email).first()
 
-    name = form['name'].data
-    email = form['email'].data
-    password = form['password'].data
+    if user is None:
+        return False
 
-    response = json.loads(signup(dict(
-        name=name,
-        email=email,
-        password=password
-    ))[0])
+    if user.token != token:
+        return False
 
-    if response['status'] == 'failure':
-        flash('error {}'.format(response['message']))
-        return redirect(url_for('auth.signup_page'))
-
-    if response['status'] != 'success':
-        flash('error An Error has Occured. Please Try Again.')
-        return redirect(url_for('auth.signup_page'))
-
-    flash('success Successfully Created Acount. Please Log In.')
-    return redirect(url_for('auth.login_page'))
+    return True
 
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login_page():
-    """Displays the login page"""
-    form = LoginForm(request.form)
-
-    if request.method == 'GET':
-        return render_template('auth/login.html', form=form)
-
-    # validate the user's input
-    if not form.validate():
-        flash('error Invalid Input Provided. Please Try Again.')
-        return redirect(url_for('auth.signup_page'))
-
-    email = form['email'].data
-    password = form['password'].data
-
-    response = json.loads(login(dict(
-        email=email,
-        password=password
-    ))[0])
-
-    if response['status'] == 'failure':
-        flash('error {}'.format(response['message']))
-        return redirect(url_for('auth.login_page'))
-
-    if response['status'] != 'success':
-        flash('error An Error has Occured. Please Try Again.')
-        return redirect(url_for('auth.login_page'))
-
-    flash('success Welcome, {}.'.format(current_user.name))
-    return redirect('/')
-
-
-@auth.route('/request/signup', methods=['POST'])
-def signup(local_input=None):
+@auth.route('/signup', methods=['POST'])
+def signup():
     """Adds a new user to the database"""
-    if local_input is None:
-        data = request.get_json()
-    else:
-        data = local_input
+    data = request.get_json()
 
     email = data['email']
     name = data['name']
     password = hash_password(data['password'])
+    token = "0"
 
     response = dict()
 
     try:
-        db.session.add(User(email=email, name=name, password=password))
+        db.session.add(User(email=email, name=name, password=password, token=token))
         db.session.commit()
 
         response['status'] = 'success'
@@ -113,50 +59,60 @@ def signup(local_input=None):
         code = 404
 
     response['code'] = code
-    return json.dumps(response), code
+    return jsonify(response), code
 
 
-@auth.route('/request/login', methods=['POST'])
-def login(local_input=None):
+@auth.route('/login', methods=['POST'])
+def login():
     """Creates a session for the current user"""
-    if local_input is None:
-        data = request.get_json()
-    else:
-        data = local_input
+    data = request.get_json()
 
     email = data['email']
     password = data['password']
 
     response = dict()
+    code = 400
 
     try:
         user = User.query.filter_by(email=email).first()
 
         if verify_password(password, user.password):
-            login_user(user)
+            user.token = binascii.b2a_hex(os.urandom(16))
+            db.session.commit()
 
+            response['email'] = user.email
+            response['token'] = user.token
             response['status'] = 'success'
             response['message'] = 'user successfully logged in'
             code = 202
         else:
             response['status']: 'failure'
             response['message']: 'invalid email or password'
+            code = 401
     except Exception as e:
         response['error'] = str(e)
-        code = 400
 
     response['code'] = code
-    return json.dumps(response), code
+    return jsonify(response), code
 
 
-@auth.route('/request/logout', methods=['GET'])
-@login_required
+@auth.route('/logout', methods=['POST'])
 def logout():
     """Deletes the current user's session"""
     response = dict()
 
+    if not validate_request(request):
+        response['status'] = 'failure'
+        response['message'] = 'invalid request'
+        code = 401
+        return jsonify(response), code
+
     try:
-        logout_user()
+        data = request.get_json()
+        user = User.query.filter_by(email=data['email']).first()
+        user.token = "0"
+        db.session.commit()
+
         response['status'] = 'success'
         response['message'] = f'logged out user'
         code = 200
@@ -166,4 +122,4 @@ def logout():
         response['error'] = str(e)
         code = 400
 
-    return json.dumps(response), code
+    return jsonify(response), code
