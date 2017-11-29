cad444fa8922611ba1dd4ca7f136401f68535dc6
jicircle <jicircle@naver.com>
2017-11-28 23:48:59 -0500

    Added Edit mark, changed tags

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from settings import init_env
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, jsonify
+from flask import Flask
 
 # create the app
 init_env()
@@ -16,11 +16,13 @@ db = SQLAlchemy(app)
 from app.controllers.auth import auth
 from app.controllers.create import create
 from app.controllers.read import read
+from app.controllers.update import update
 from app.controllers.delete import delete
 
 app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(create, url_prefix='/create')
 app.register_blueprint(read, url_prefix='/read')
+app.register_blueprint(update, url_prefix='/update')
 app.register_blueprint(delete, url_prefix='/delete')
 
 
